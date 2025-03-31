c2b84e7bab31589368e51e87abe6b0ca29237be7
Stanford <astrelin2001@gmail.com>
2025-04-01 00:43:52 +0300

    добавил tts pyttsx3

---
+++ tts_apis/tts_pyttsx3.py
@@ -0,0 +1,20 @@
+import pyttsx3
+import time
+
+# https://pypi.org/project/pyttsx3/
+# https://github.com/nateshmbhat/pyttsx3
+# model: eSpeak NG или SAPI5
+# Языки: Русский, Английский
+# Работает быстро и без ошибок
+def create_pyttsx3_audio(text):
+    engine = pyttsx3.init()
+    #engine.setProperty('voice', 'russian')  # Указываем русский голос
+    name = "pytttsx3_" + str(time.time() * 1000) + ".mp3"
+    engine.save_to_file(text, name)
+    engine.runAndWait()
+    return name
+
+# проверки работоспособности
+# using_pyttsx3_tts("нежданно негаданно")
+# using_pyttsx3_tts("english text sounds")
+# using_pyttsx3_tts("hi")
