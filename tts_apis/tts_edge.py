d173f580a9542a22fcd7a592eb98056b46dba3f6
Stanford <astrelin2001@gmail.com>
2025-04-05 12:12:01 +0300

    добавил новый tts

---
+++ tts_apis/tts_edge.py
@@ -0,0 +1,25 @@
+import edge_tts
+import asyncio
+import os
+import time
+
+# https://github.com/rany2/edge-tts
+# Языки: Русский(можно и английский если выбрать нужный голос)
+async def async_create_edge_audio(text):
+    try:
+        communicate = edge_tts.Communicate(text=text, voice="ru-RU-SvetlanaNeural")
+        # путь к папке со сгенерированными файлами
+        path = os.getcwd() + "\\generated_audios\\"
+        # имя сгенерированного файла
+        filename = "edge_" + str(time.time() * 1000) + ".mp3"
+        # полный путь
+        path = path + filename
+        await communicate.save(path)
+        return filename
+    except Exception as e:
+        print(f"An error occurred while using Edge TTS: {e}")
+        return None
+
+# Тесты
+# asyncio.run(async_create_edge_audio("Привет"))   
+
